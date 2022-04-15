20bf544d84c558365e3120f0beafe01bcb29077f
leunsoo <98530935+leunsoo@users.noreply.github.com>
2022-04-15 14:13:46 +0900

    Picker 제품구분1 아이템 셋

---
+++ iljin_m/iljin_m/ViewModels/ItemDiv1ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using iljin_m.Models;
+using iljin_m.Services;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace iljin_m.ViewModels
+{
+    public class ItemDiv1ViewModel : Notify
+    {
+        //제품구분1 
+        private Dictionary<string, string> itemDiv1 = new Dictionary<string, string>() { { "PET","I0002" }, { "PP","I0003" }, { "알미늄","I0004" } };
+
+        //ItemSource
+        public List<KeyValuePair<string,string>> ItemDiv1List
+        {
+            get => itemDiv1.ToList();
+        }
+
+        public KeyValuePair<string,string> SelectedItem { get; set; }
+    }
+}
---
+++ iljin_m/iljin_m/ViewModels/ItemDiv2ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using iljin_m.Models;
+using iljin_m.Services;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace iljin_m.ViewModels
+{
+    public class ItemDiv2ViewModel : Notify
+    {                  
+    }
+}
